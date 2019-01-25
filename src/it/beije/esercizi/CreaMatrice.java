d32e4f392ade2e21f8359e0621fd6b05deb42787
ivfly <ivo.mosca@privategriffe.com>
2019-01-25 17:56:46 +0100

    creazione di matrice di array da argomenti

---
+++ src/it/beije/esercizi/CreaMatrice.java
@@ -0,0 +1,23 @@
+package it.beije.esercizi;
+
+import java.util.Arrays;
+
+public class CreaMatrice {
+
+	public static void main(String[] args) {
+		//inizializzo array
+		char[][] array = new char[args.length][];
+		
+		//for (int i = 0; i < args.length; i++) {
+		for (int i = 0; i < array.length; i++) {//vanno bene entrambe
+			array[i] = new char[args[i].length()];
+			
+			for (int j = 0; j < args[i].length(); j++) {
+				array[i][j] = args[i].charAt(j);
+			}
+			
+			System.out.println(Arrays.toString(array[i]));
+		}
+	}
+
+}
