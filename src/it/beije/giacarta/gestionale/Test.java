bbea635cf02aac861506d973c78e8a43289ac19d
LaMonk95 <work.lucianolamonica@gmail.com>
2019-02-19 17:26:02 +0100

    modifica classe tet prova commit

--- src/it/beije/giacarta/gestionale/Test.java
+++ src/it/beije/giacarta/gestionale/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		//il nome del dipende delle risorse umane
 		RisorseUmane r = new RisorseUmane("mario", 0, "a", "b", "2569");
-		
+		System.out.println("sto provando");
 
 	}
 
